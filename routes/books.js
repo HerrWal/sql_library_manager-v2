f9fd5e1c60f43a1f0d04c642276cbbf1954b8172
HerrWal <85037202+HerrWal@users.noreply.github.com>
2024-09-18 23:18:40 -0400

    Sep 18
    Add New, Create and Update routes

--- routes/books.js
+++ routes/books.js
@@ -14,14 +14,33 @@ function asyncHandler(cb) {
   }
 }
 
-/* GET all books. */
-router.get('/', asyncHandler(async(req, res, next) => {
+// Full list of books
+router.get('/', asyncHandler(async(req, res) => {
     const books = await Book.findAll();
-    console.log("Books:", books);
-    res.render('index', { books, title: 'Books'});
-  }
-));
+    res.render('index', { books, title: 'Books'});  
+}));
 
+// New Book 
+router.get('/new', (req, res) => {
+  res.render('new-book', { title: "New Book" });
+});
 
+// Create Book 
+router.post('/books/new', asyncHandler(async(req, res) => {
+  console.log(req.body);
+    const book = await Book.create(req.body);
+    console.log('Submitted: ' + book.title);
+    res.redirect('/books/' + book.id);  
+}));
+
+// Book Detail
+router.get('/:id', asyncHandler(async(req, res) => {
+    res.render('update-book', { title: 'Book Details' });  
+}));
+
+// Update Book
+router.post('/books/:id', asyncHandler(async (req, res) => {
+  res.redirect('/books')
+}));
 
 module.exports = router;
