cee8a38eee328f24ed341517f893ee668089b52a
HerrWal <85037202+HerrWal@users.noreply.github.com>
2024-11-04 21:00:06 -0500

    11-4-24

--- routes/books.js
+++ routes/books.js
@@ -2,6 +2,7 @@ var express = require('express');
 const { render } = require('../app');
 var router = express.Router();
 const { Book } = require('../models');
+const { Op } = require('sequelize');
 
 // Handler function to wrap each route
 function asyncHandler(cb) {
@@ -14,10 +15,26 @@ function asyncHandler(cb) {
   }
 }
 
-// Full list of books
+// Full list of books with optional search
 router.get('/', asyncHandler(async(req, res) => {
-    const books = await Book.findAll();
-    res.render('index', { books, title: 'Books'});  
+  const { query } = req.query;
+  let books;
+  if (query) {
+    books = await Book.findAll({
+      where: {
+        [Op.or]: [
+          { title: { [Op.like]: `%${query}%` } },
+          { author: { [Op.like]: `%${query}%` } },
+          { genre: { [Op.like]: `%${query}%` } },
+          { year: { [Op.like]: `%${query}%` } }
+        ]
+      }
+    });
+  } else {
+      books = await Book.findAll();
+  }
+
+  res.render('index', { books, title: 'Books'});  
 }));
 
 // New Book 
